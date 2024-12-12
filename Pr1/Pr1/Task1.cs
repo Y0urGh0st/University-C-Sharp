50c7094a6f9ce7577dd2c647c80345e5099d63e8
Y0urGh0st <146064939+Y0urGh0st@users.noreply.github.com>
2024-12-12 21:09:20 +0200

    Add files via upload

---
+++ Pr1/Pr1/Task1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TaskNamespace
+{
+    public class Task1
+    {
+        public void Execute()
+        {
+            const int serialNumber = 11;
+
+            int num = serialNumber % 10;
+            int min = 1;
+            int max = 10 + num;
+
+            Console.WriteLine("Enter three numbers:");
+            if (!int.TryParse(Console.ReadLine(), out int a) ||
+                !int.TryParse(Console.ReadLine(), out int b) ||
+                !int.TryParse(Console.ReadLine(), out int c))
+            {
+                Console.WriteLine("Invalid input. Please enter valid numbers.");
+                return;
+            }
+
+            Console.WriteLine($"Numbers that belong to the interval [{min},{max}]:");
+            if (IsInRange(a, min, max)) Console.WriteLine(a);
+            if (IsInRange(b, min, max)) Console.WriteLine(b);
+            if (IsInRange(c, min, max)) Console.WriteLine(c);
+        }
+
+        private bool IsInRange(int number, int min, int max)
+        {
+            return number >= min && number <= max;
+        }
+    }
+}
---
+++ Pr1/Pr1/Task3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TaskNamespace
+{
+    public class Task3
+    {
+        public void Execute()
+        {
+            const int serialNumber = 11;
+            int num = serialNumber % 10;
+            int n = 10 + num;
+
+            int[] array = new int[n];
+            Console.WriteLine("Enter " + n + " array elements:");
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int min = int.MaxValue;
+            int max = int.MinValue;
+
+            Console.WriteLine("Array:");
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+                if (item < min) min = item;
+                if (item > max) max = item;
+            }
+
+            Console.WriteLine("\nMin value: " + min);
+            Console.WriteLine("Max value: " + max);
+        }
+    }
+}
